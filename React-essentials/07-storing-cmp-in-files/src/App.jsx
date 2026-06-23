ac2d891b143fa78f4a017b14b5b0de3e587992fc
Toqeer Sheikh <iamtoqeer05@gmail.com>
2026-06-23 06:49:26 +0500

    07-storing-cmp-in-files

---
+++ React-essentials/07-storing-cmp-in-files/src/App.jsx
@@ -0,0 +1,32 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              image={CORE_CONCEPTS[0].image}
+              title="Components"
+              description="The core UI building block - compose the user interface by combining multiple components."
+            />
+            <CoreConcepts
+              image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
