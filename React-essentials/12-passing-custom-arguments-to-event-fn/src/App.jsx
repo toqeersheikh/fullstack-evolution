d886915deec24db0c2fb2586a3cca86ffb20b176
Toqeer Sheikh <iamtoqeer05@gmail.com>
2026-06-24 22:52:04 +0500

    12-passing-custom-arguments-to-event-fn

---
+++ React-essentials/12-passing-custom-arguments-to-event-fn/src/App.jsx
@@ -0,0 +1,49 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton : 'JSX', 'Component', 'State', 'props'
+    console.log(`Tab selected ${selectedButton}`);
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              image={CORE_CONCEPTS[0].image}
+              title="Components"
+              description="The core UI building block - compose the user interface by combining multiple components."
+            />
+            <CoreConcepts
+              image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            {/* <TabButton label="State"></TabButton> */}
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
