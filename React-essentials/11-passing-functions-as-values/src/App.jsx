d579a963f9ca4b16f230cfb1075e1c49a952f9eb
Toqeer Sheikh <iamtoqeer05@gmail.com>
2026-06-24 20:49:36 +0500

    11-passing-functions-as-values

---
+++ React-essentials/11-passing-functions-as-values/src/App.jsx
@@ -0,0 +1,46 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  function handleSelect() {
+    console.log("Button Clicked - Tab selected");
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              image={CORE_CONCEPTS[0].image}
+              title="Components"
+              description="The core UI building block - compose the user interface by combining multiple components."
+            />
+            <CoreConcepts
+              image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+            {/* <TabButton label="State"></TabButton> */}
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
