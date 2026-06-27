6274c8186d5e406839008f7d08cdf6fb1ba0a112
Toqeer Sheikh <iamtoqeer05@gmail.com>
2026-06-27 15:47:17 +0500

    15-deriving-computing-values

---
+++ React-essentials/15-deriving-computing-values/src/App.jsx
@@ -0,0 +1,62 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
+import { EXAMPLES } from "./data";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
+  function handleSelect(selectedButton) {
+    // selectedButton : 'JSX', 'Component', 'State', 'props'
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              image={CORE_CONCEPTS[0].image}
+              title="Components"
+              description="The core UI building block - compose the user interface by combining multiple components."
+            />
+            <CoreConcepts
+              image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            {/* <TabButton label="State"></TabButton> */}
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
