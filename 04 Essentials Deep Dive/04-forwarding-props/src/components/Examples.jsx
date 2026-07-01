0c1d41ba0cd865faae464143986bc3a5272185eb
Toqeer Sheikh <iamtoqeer05@gmail.com>
2026-07-01 17:49:24 +0500

    04-forwarding-props

---
+++ 04 Essentials Deep Dive/04-forwarding-props/src/components/Examples.jsx
@@ -0,0 +1,56 @@
+import { EXAMPLES } from "../data";
+import Section from "./Section";
+import TabButton from "./TabButton";
+import { useState } from "react";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton : 'JSX', 'Component', 'State', 'props'
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onClick={() => handleSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onClick={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onClick={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onClick={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+
+      {!selectedTopic ? (
+        <p>Please Select a topic.</p>
+      ) : (
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+      )}
+    </Section>
+  );
+}
